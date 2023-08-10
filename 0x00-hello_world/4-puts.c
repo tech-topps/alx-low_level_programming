16f4ff67acabc6b60112cdc9250a9a5ecb873a2c
tech-topps <temitopeadebisi40@yahoo.com>
2023-08-10 16:28:15 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - main function
+ * main - Entry point
  *
- * Return: always 0
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
