b42667f97582f7e2589dc67df4414eafc7fefeec
tech-topps <temitopeadebisi40@yahoo.com>
2023-08-13 16:39:22 -0700

    Print

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Empty point
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+char i;
+
+for (i = 'z' ; i >= 'a'; i--)
+	putchar(i);
+putchar('\n');
+return (0);
+}
