27bde99b4627dc20601574500e9086e702b5491f
tech-topps <temitopeadebisi40@yahoo.com>
2023-08-13 14:43:36 -0700

    Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Empty point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char i;
+
+for (i = 'a' ; i <= 'z' ; i++)
+	putchar(i);
+putchar("\n");
+return (0);
+}
