fa92365df9cda58990fa7d22f1e9575451408134
tech-topps <temitopeadebisi40@yahoo.com>
2023-08-13 15:36:14 -0700

    Alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - printing all letters except q and e
+ * main - Empty point
  *
- * Result: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
