98c4582b7cc79518e31ef1e1652687e819bf1d77
tech-topps <temitopeadebisi40@yahoo.com>
2023-08-15 17:42:09 -0700

    times_table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+void times_table(void)
+{
+int a, b, result;
+
+for (a = 0 ; a < 10 ; a++)
+{
+for (b = 0 ; b < 10 ; b++)
+{
+result = a * b;
+if (b == 0)
+	printf("%d, ", result);
+else
+{
+printf("%2d", result);
+if (b != 9)
+	printf(", ");
+}
+}
+printf("\n");
+}
+}
