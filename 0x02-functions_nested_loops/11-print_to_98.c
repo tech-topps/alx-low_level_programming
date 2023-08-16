93f046890c289457bfee982bf6ac99275dacd995
tech-topps <temitopeadebisi40@yahoo.com>
2023-08-15 19:49:00 -0700

    print_to_98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+void print_to_98(int n)
+{
+int a;
+
+if (n >= 0 && n <= 98)
+{
+for (a = n ; a <= 98 ; a++)
+{
+printf("%d", a);
+}
+printf("\n");
+{
+	else if (n < 0)
+}
+}
+}
