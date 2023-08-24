910926cc2a9467d3ecd656cf6c83e5b81554a7d5
tech-topps <temitopeadebisi40@yahoo.com>
2023-08-24 15:25:53 -0700

    string_toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - function that changes all lowercase to uppercase
+ * @n: parameter pointer
+ * Return: n
+ */
+
+char *string_toupper(char *n)
+{
+int b;
+
+b = 0;
+while (n[b] != '\0')
+{
+if (n[b] >= 'a' && n[b] <= 'z')
+	n[b] = n[b] - 32;
+b++;
+}
+return (n);
+}
