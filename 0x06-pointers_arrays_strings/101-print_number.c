6c2289a4803db91512691ab8a74219cd73d588e8
tech-topps <temitopeadebisi40@yahoo.com>
2023-08-25 18:04:11 -0700

    print_number

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_number - function that prints an integer
+ * @n: parameter integer
+ * Return: Always 0
+ */
+
+void print_number(int n)
+{
+unsigned int a1;
+
+a1 = n;
+if (n < 0)
+{
+_putchar('-');
+a1 = -n;
+}
+if (a1 / 10 != 0)
+{
+print_number(a1 / 10);
+}
+_putchar((a1 % 10) + '0');
+}
