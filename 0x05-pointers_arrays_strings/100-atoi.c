1de94481df32621694a8fccab106378606e4fa74
tech-topps <temitopeadebisi40@yahoo.com>
2023-09-01 16:54:36 -0700

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,6 +1,36 @@
 #include "main.h"
 
+/**
+ * _atoi - function that convert a string to an integar
+ * @s: parameter pointer to convert
+ * Return: a integer
+ */
+
 int _atoi(char *s)
 {
+int a = 0;
+unsigned int b = 0;
+int i = 1;
+int j = 0;
 
+while (s[a])
+{
+if (s[a] == 45)
+{
+i *= -1;
+}
+while (s[a] >= 48 && s[a] <= 57)
+{
+j = 1;
+b = (b * 10) + (s[a] - '0');
+a++
+}
+if (j == 1)
+{
+break;
+}
+a++;
+}
+b *= i;
+return (b);
 }
