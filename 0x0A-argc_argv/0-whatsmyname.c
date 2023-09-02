6e07788d433e6ebf99be4bcff8da7e34069869f8
tech-topps <temitopeadebisi40@yahoo.com>
2023-09-01 20:46:37 -0700

    My_new_name_is

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - function that print its name
+ * @argc: parameter argument count
+ * @argv: parameter argument vector
+ * Return: 0
+ */
+
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
