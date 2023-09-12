e028c365dafe1609e46c4f2d08cd37045ff95042
tech-topps <temitopeadebisi40@yahoo.com>
2023-09-11 17:38:13 -0700

    dog_h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure dog
+ * @name: parameter name of the dog
+ * @age: parameter age of the dog
+ * @owner: parameter owner of the dog
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif
